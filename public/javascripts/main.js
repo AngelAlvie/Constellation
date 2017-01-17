6e4947ab72334e7e3fb60d4029132c885fd34f84
Angel Alvarez <alvareza@mit.edu>
2017-01-17 14:28:30 -0500

    Made the client side actually OKAY

---
+++ public/javascripts/main.js
@@ -0,0 +1,30 @@
+var resize = function() {
+  var screenWidth = $(window).width();
+  var screenHeight = $(window).height();
+  var breakpoints = [768, 992, 1170];  //xs, sm, md, lg
+  var hiddenWidth = 0;
+  var hiddenHeight = 200;
+  if ( screenWidth < breakpoints[0] ) { //extra small screen
+    hiddenWidth = screenWidth * 0.9;
+  } else if ( screenWidth < breakpoints[1] ) { // small screen
+    hiddenWidth = screenWidth * 0.75;
+  } else if ( screenWidth < breakpoints[2] ) { // medium
+    hiddenWidth = screenWidth * .6;
+  } else { // bigger than medium (a.k.a large)
+    hiddenWidth = screenWidth * .45;
+  }
+  return [hiddenWidth, hiddenHeight];
+};
+
+
+$(document).ready(function() {
+  var tmp = resize();
+  $(".hidden").css("width",  tmp[0]);
+  $(".hidden").css("height", 0);
+  $(".hidden").animate({height: 200}, 600);
+});
+$( window ).resize(function() {
+
+  var tmp = resize();
+  $(".hidden").stop(true).animate({width: tmp[0], height: tmp[1]}, 200);
+});
