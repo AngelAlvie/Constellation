7f87969f30c4f3aacdc32100a559f53409432750
Angel Alvarez <alvareza@mit.edu>
2017-01-18 19:50:26 -0500

    made scrollbar and slight positioning tweaks

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -25,12 +25,16 @@ var resize = function() {
   var hiddenHeight = 200;
   if ( screenWidth < breakpoints[0] ) { //extra small screen
     hiddenWidth = screenWidth * 0.9;
+    hiddenHeight = screenHeight*0.55;
   } else if ( screenWidth < breakpoints[1] ) { // small screen
     hiddenWidth = screenWidth * 0.75;
+    hiddenHeight = screenHeight*0.5;
   } else if ( screenWidth < breakpoints[2] ) { // medium
-    hiddenWidth = screenWidth * .6;
+    hiddenWidth = screenWidth * 0.6;
+    hiddenHeight = screenHeight*0.45;
   } else { // bigger than medium (a.k.a large)
-    hiddenWidth = screenWidth * .45;
+    hiddenWidth = screenWidth * 0.45;
+    hiddenHeight = screenHeight*0.4;
   }
   return [hiddenWidth, hiddenHeight];
 };
@@ -40,7 +44,7 @@ $(document).ready(function() {
   var tmp = resize();
   $(".hidden").css("width",  tmp[0]);
   $(".hidden").css("height", 0);
-  $(".hidden").animate({height: 200}, 600);
+  $(".hidden").animate({height: tmp[1]}, 600);
   renderClickedButtons();
 });
 $( window ).resize(function() {
