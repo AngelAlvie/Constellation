fdbdb60b360d54f5b542e210371c70e1414660ea
Thad <thad.megchelsen@gmail.com>
2017-01-21 00:29:02 -0500

    Start work on derpy profile page.

---
+++ public/javascripts/profile.js
@@ -0,0 +1,40 @@
+
+var resize = function() {
+  var screenWidth = $(window).width();
+  var screenHeight = $(window).height();
+  var breakpoints = [768, 992, 1170];           //xs, sm, md, lg
+  var hiddenWidth = 0;
+  var hiddenHeight = 200;
+  if ( screenWidth < breakpoints[0] ) {         // extra small screen
+    hiddenWidth = screenWidth * 0.9;
+    hiddenHeight = screenHeight*0.55;
+  } else if ( screenWidth < breakpoints[1] ) {  // small screen
+    hiddenWidth = screenWidth * 0.75;
+    hiddenHeight = screenHeight*0.5;
+  } else if ( screenWidth < breakpoints[2] ) {  // medium
+    hiddenWidth = screenWidth * 0.6;
+    hiddenHeight = screenHeight*0.45;
+  } else {                                      // large
+    hiddenWidth = screenWidth * 0.45;
+    hiddenHeight = screenHeight*0.4;
+  }
+  return [hiddenWidth, hiddenHeight];
+};
+
+$(document).ready(function() {
+  var tmp = resize();
+  $(".hidden").css("width",  tmp[0]);
+  $(".hidden").css("height", 0);
+  $(".hidden").animate({height: tmp[1]}, 600);
+  renderClickedButtons();
+});
+
+$( window ).resize(function() {
+
+  var tmp = resize();
+  $(".hidden").stop(true).animate({width: tmp[0], height: tmp[1]}, 200);
+});
+
+var backClicked = function() {
+  window.location.assign('/search');
+};
