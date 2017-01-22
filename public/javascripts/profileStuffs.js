1cd583d23a9f37ef9ec7b2a8edc11cf3ef534407
Thad <thad.megchelsen@gmail.com>
2017-01-21 21:54:48 -0500

    add profile deep stuff pages, add content to those, make them work and like route them

--- public/javascripts/profileStuffs.js
+++ public/javascripts/profileStuffs.js
@@ -5,18 +5,18 @@ var resize = function() {
   var breakpoints = [768, 992, 1170];           //xs, sm, md, lg
   var hiddenWidth = 0;
   var hiddenHeight = 200;
-  if ( screenWidth < breakpoints[0] ) {         // extra small screen
+  if ( screenWidth < breakpoints[0] ) {         //extra small screen
     hiddenWidth = screenWidth * 0.9;
-    hiddenHeight = screenHeight*0.6;
+    hiddenHeight = screenHeight * 0.9;
   } else if ( screenWidth < breakpoints[1] ) {  // small screen
     hiddenWidth = screenWidth * 0.75;
-    hiddenHeight = screenHeight*0.55;
+    hiddenHeight = screenHeight * 0.9;
   } else if ( screenWidth < breakpoints[2] ) {  // medium
-    hiddenWidth = screenWidth * 0.6;
-    hiddenHeight = screenHeight*0.5;
-  } else {                                      // large
-    hiddenWidth = screenWidth * 0.45;
-    hiddenHeight = screenHeight*0.45;
+    hiddenWidth = screenWidth * .6;
+    hiddenHeight = screenHeight * 0.9;
+  } else {                                     // large
+    hiddenWidth = screenWidth * .45;
+    hiddenHeight = screenHeight * 0.9;
   }
   return [hiddenWidth, hiddenHeight];
 };
@@ -36,25 +36,5 @@ $( window ).resize(function() {
 });
 
 var backClicked = function() {
-  window.location.assign('/search');
-};
-
-var nebulaClicked = function() {
-  window.location.assign('/nebula');
-};
-
-var myConstellationsClicked = function() {
-  window.location.assign('/myConstellations');
-};
-
-var myStarsClicked = function() {
-  window.location.assign('/myStars');
-};
-
-var savedConstellationsClicked = function() {
-  window.location.assign('/savedConstellations');
-};
-
-var savedStarsClicked = function() {
-  window.location.assign('/savedStars');
+  window.location.assign('/profile');
 };
